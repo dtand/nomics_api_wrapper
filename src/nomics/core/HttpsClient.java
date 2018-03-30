1e37db6907cc0d5d1bff303988d4b64f52a958e0
dtand <daniel@igotozarks.com>
2018-03-29 23:37:19 -0400

    added one additional package up - nomics

--- src/nomics/core/HttpsClient.java
+++ src/nomics/core/HttpsClient.java
@@ -1,4 +1,4 @@
-package core;
+package nomics.core;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- src/nomics/core/NomicsMarkets.java
+++ src/nomics/core/NomicsMarkets.java
@@ -1,4 +1,4 @@
-package core;
+package nomics.core;
 import java.io.IOException;
 
 import org.json.JSONArray;
